976e50a0f0d8be6f438f2175111d4f741d9151b5
小黑 <2908436596@qq.com>
2023-03-06 10:50:33 +0800

    3/5

--- day6/6-02创建项目和介绍Bootstrap/main.go
+++ day6/6-02创建项目和介绍Bootstrap/main.go
@@ -22,20 +22,34 @@ var Publishers = []string{"人民出版社", "北京出版社"}
 func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/book_add", add)
+	http.HandleFunc("/book_edit", edit)
+	http.HandleFunc("/book_delete", delete)
 	http.HandleFunc("/book_list", list)
-	http.ListenAndServe("localhost:8080", nil)
+	http.Handle("/statics/", http.FileServer(http.Dir(".")))
+	http.ListenAndServe("localhost:8888", nil)
 }
 
 func list(w http.ResponseWriter, r *http.Request) {
 	Books = append(Books, &Book{"人生", 19.8, "北京出版社", "2021-10-22", []string{"张三", "李四"}})
 	//tmp, _ := template.ParseFiles("home.html", "list.html")
 	tmp, _ := template.New("list.html").Funcs(template.FuncMap{"add":addOne}).ParseFiles("home.html", "list.html")
-	tmp.Execute(w, Books)
+	tmp.Execute(w,Books)
 	
 }
 
 func addOne(a, b int) int{
 	return a + b
+}
+
+func isIn(a string, list []string) bool {
+	for _, s := range list {
+		if s == a {
+			return true
+		}
+	}
+
+	return false
+
 }
 
 func add(w http.ResponseWriter, r *http.Request) {
@@ -62,7 +76,47 @@ func add(w http.ResponseWriter, r *http.Request) {
 	
 }
 
+func edit(w http.ResponseWriter, r *http.Request) {
+	idStr := r.URL.Path[len("book_edit/"):]
+	id, _ := strconv.Atoi(idStr)
+	if(id >= len(Books)) {
+		http.NotFound(w, r)
+		return
+	}
+	switch r.Method {
+	case "GET":
+		tmp, _ := template.New("edit.html").Funcs(template.FuncMap{"isIn":isIn}).ParseFiles("home.html", "edit.html")
+		info := map[string]interface{} {"authors":Authors,"publishers":Publishers,"id":id,"books":Books[id]}
+		tmp.ExecuteTemplate(w ,"edit.html", info)
+	case "POST":
+		//保存数据
+		book := Books[id]
+		price, _ := strconv.ParseFloat(r.FormValue("price"), 64)
+
+		book.Price = price
+		book.Title = r.FormValue("title")
+		book.PublishDate = r.FormValue("publish_date")
+		book.Publisher = r.FormValue("publisher")
+		book.Authors = r.PostForm["authors"]
+		http.Redirect(w, r, "/list", 301)
+
+	}
+	
+	
+}
+
+func delete(w http.ResponseWriter, r *http.Request) {
+	idStr := r.URL.Path[len("book_edit/"):]
+	id, _ := strconv.Atoi(idStr)
+	if(id >= len(Books)) {
+		http.NotFound(w, r)
+		return
+	}
+	Books = append(Books[:id],Books[id+1:]...)
+	http.Redirect(w, r, "/book_list", 301)
+}
+
 func home(w http.ResponseWriter, r *http.Request) {
 	tmp, _ := template.ParseFiles("home.html")
-	tmp.Execute(w, Books)
+	tmp.Execute(w, nil)
 }
